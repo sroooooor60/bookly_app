2b6446342324f3cf01ee959aee7cb341e2ec4034
sroooooor60 <sroooooor90@gmail.com>
2025-05-13 21:45:42 +0300

    finish custom app bar

---
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,33 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [CustomAppBar()]);
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 50),
+      child: Row(
+        children: [
+          Image.asset(AssetsData.logo, height: 24),
+          Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(FontAwesomeIcons.magnifyingGlass, size: 24),
+          ),
+        ],
+      ),
+    );
+  }
+}

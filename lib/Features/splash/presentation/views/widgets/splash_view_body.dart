b5366e5f8d3ac0267686ee29467a15197edae63a
sroooooor60 <sroooooor90@gmail.com>
2025-05-13 21:02:53 +0300

    navigat to HomeView & refactor to initStat

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_app/Features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/Features/splash/presentation/views/widgets/slide_text.dart';
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -17,21 +20,9 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
+    initSlidingAnimation();
 
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    );
-
-    slidingAnimation = Tween<Offset>(
-      begin: Offset(0, 5),
-      end: Offset.zero,
-    ).animate(animationController);
-    animationController.forward();
-    //حذفنا الليسنر علشان حطينا الأنيميتد بيلدر
-    // animationController.addListener(() {
-    //   setState(() {});
-    // });
+    navigatToHome();
   }
 
   @override
@@ -50,5 +41,32 @@ class _SplashViewBodyState extends State<SplashViewBody>
         SlidText(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: Offset(0, 3),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+    //حذفنا الليسنر علشان حطينا الأنيميتد بيلدر
+    // animationController.addListener(() {
+    //   setState(() {});
+    // });
+  }
+
+  void navigatToHome() {
+    Future.delayed(Duration(seconds: 3), () {
+      Get.to(
+        () => const HomeView(),
+        transition: Transition.zoom,
+        duration: kDurationTransition,
+      );
+    });
   }
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color.fromARGB(255, 33, 30, 88);
+const kDurationTransition = Duration(milliseconds: 300);

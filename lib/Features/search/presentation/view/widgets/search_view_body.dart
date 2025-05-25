2753126b36c78489dc93892d83f3b7d4e35b6cb6
sroooooor60 <sroooooor90@gmail.com>
2025-05-25 23:17:43 +0300

    finish Search view and ALL UI DONE

--- lib/Features/search/presentation/view/widgets/search_view_body.dart
+++ lib/Features/search/presentation/view/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/Features/search/presentation/view/widgets/custom_search_text_field.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -8,7 +10,35 @@ class SearchViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(children: [CustomSearchTextField()]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomSearchTextField(),
+          const SizedBox(height: 20),
+          const Text('Search Results', style: Styles.textStyle18),
+          const SizedBox(height: 10),
+
+          Expanded(child: ResultSearchListView()),
+        ],
+      ),
+    );
+  }
+}
+
+class ResultSearchListView extends StatelessWidget {
+  const ResultSearchListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: const BestSellerListViewItem(),
+        );
+      },
     );
   }
 }
